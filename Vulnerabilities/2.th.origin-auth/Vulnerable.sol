4ca760543312cc5b30650a6df246ff21217417b1
Nicolas-Andrei Manasia <manasia2006nicolas@gmail.com>
2025-10-03 10:59:40 +0300

    Update and rename Counter.sol to Vulnerable.sol

--- Vulnerabilities/2.th.origin-auth/Vulnerable.sol
+++ Vulnerabilities/2.th.origin-auth/Vulnerable.sol
@@ -1,3 +1,10 @@
+/*
+What is tx.origin? Ussualy people believe is the same thing with msg.sender, which is an address that tells which person/contract deployed the contract in question. But tx.origin isn’t the same thing. The formal definition would be:
+
+tx.origin = a global EVM variable that returns the original externally-owned account (EOA) that started the transaction.
+
+But the simpler explanation is the first one that deployed the contract.
+*/
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
@@ -12,18 +19,5 @@ contract Vulnarable {
         ///This should check if the owner deployed, but it only checks if the person that deployed the first contract is the owner.
         payable(tx.origin).call{ value : amount}("");
         ///So, if a intermedary contract tricks some other contract to be deployed, the owner will not be that one, so anyone can use it.
-    } 
-}
-
-contract Safe {
-    address owner;
-    constructor() public {
-        owner = msg.sender;
-    }
-
-    function withdraw(uint256 amount) public {
-        require(msg.sender == owner, "Not owner");
-        ///Always use msg.sender
-        payable(tx.origin).call{ value : amount}("");
     } 
 }
