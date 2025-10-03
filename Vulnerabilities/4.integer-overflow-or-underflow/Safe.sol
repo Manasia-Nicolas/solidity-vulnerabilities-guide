b00b3db7ff932e8d08191e4f61662ba87aacd881
Nicolas-Andrei Manasia <manasia2006nicolas@gmail.com>
2025-10-03 11:06:01 +0300

    Update Safe.sol

--- Vulnerabilities/4.integer-overflow-or-underflow/Safe.sol
+++ Vulnerabilities/4.integer-overflow-or-underflow/Safe.sol
@@ -1,3 +1,19 @@
 /*
 Luckly Solidity with a version bigger then 0.8.0 would revert if an overflow or underflow would happen. You can also use Safemath for older versions.
 */
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13; ///Solidity >= 0.8.0
+
+contract Safe {
+    mapping(address => uint256) public balances;
+
+    constructor(uint256 amount) public {
+        balances[msg.sender] = amount;
+    }
+
+    function transfer(address to, uint256 amount) public{
+        require(balances[msg.sender] >= amount, "Not enough balance");
+        balances[to] += amount;
+        balances[msg.sender] -= amount;
+    }
+}
