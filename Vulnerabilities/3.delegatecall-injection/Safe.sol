9a47f149c77173574f6313a4988d8807226a4295
Nicolas-Andrei Manasia <manasia2006nicolas@gmail.com>
2025-10-03 11:03:17 +0300

    Create Safe.sol

---
+++ Vulnerabilities/3.delegatecall-injection/Safe.sol
@@ -0,0 +1,5 @@
+/*
+There are two things that someone can do to solve this issue:
+1. Make it so that the msg.sender is the owner
+2. Allows only a whitelisted set of function selectors to be forwarded.
+*/
